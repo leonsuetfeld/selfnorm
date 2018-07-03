2b22f97be7a9de967246c2573d06226aa74b8842
leonsuetfeld <mail@leonsuetfeld.com>
2018-07-03 14:27:57 +0200

    analysis file updated

--- deepnet_supervisor_analysis.py
+++ deepnet_supervisor_analysis.py
@@ -36,7 +36,7 @@ os.system("nvidia-smi")
 
 command = "python3 "                + 'deepnet_main.py' + \
           " -path_relative "        + './' + \
-          " -experiment_name="      + 'debugging' + \
+          " -experiment_name="      + 'selfnorm_smcn' + \
           " -mode="                 + 'analysis' + \
           " -task="                 + 'cifar100'
 subprocess.run(command, shell=True)
